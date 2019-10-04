1e085ddda3b2b60b3def770ae2718121ec69c1d6
Miles Van Denburg <54696014+MVanDenburg92@users.noreply.github.com>
2019-10-04 19:16:22 -0400

    Update to API call.
    No UI elements, but we can call the data now and produce it for our manipulation.

--- Script2.py
+++ Script2.py
@@ -1,33 +1,23 @@
-# Place necessary comments and code here. 
 import requests
 
-# cityname = raw_input('Please input your City (Example: Worcester:)')
-
+cityname = raw_input('Please input your City (Example: Worcester:)')
+#
 # OPENWEATHERMAP_API_KEY = os.environ.get('dc97005f21d5744e0fdd1e405d09115b')
 
-OWMAP_APIKEY = 'dc97005f21d5744e0fdd1e405d09115b'
+apikey = 'dc97005f21d5744e0fdd1e405d09115b'
 
-url = 'https://samples.openweathermap.org/data/2.5/weather'
 
-params = {'q':'London,uk','appid': OWMAP_APIKEY}
+url = 'https://api.openweathermap.org/data/2.5/weather/?q=%s&units=metric&appid=%s' % urlencode(cityname, apikey)
+#Example of complete URL https://api.openweathermap.org/data/2.5/weather/?q=Chicago&units=metric&appid=dc97005f21d5744e0fdd1e405d09115b
+#Validated at https://codebeautify.org/jsonviewer
+##params = {'q':city_name,'appid': OWMAP_APIKEY}
 
-response = requests.get(url=url, params=params)
-business = response.json()
-print business
+response = requests.get(url)
+weatherdata = response.json()
+print weatherdata
 #JSON is a nested hierarchy file and in order to call the data you need to do a request.get and define your arguments
 
-rating = business['coord']['lat']
+results = weatherdata['coord']#['lat']
 
-print rating
-###'''page = requests.get(url)
-##soup = BeautifulSoup(page.text,"html.parser")
-###city_results = []
-
-# Locate elements on page to be scraped
-# findAll() locates all occurrences of div tag with the given class name
-# stores it in the BeautifulSoup object
-# all_results = soup.findall("forecast-list")
-# all_results = all_results.text
-# print all_results
-###for div in soup.findAll("div",{"class":"container"}):
-   ###print div.text
+print results
+###The data is called and returned 
